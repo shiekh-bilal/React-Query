2363c0d1e3a0e02343fcddedce43f8ac16aae542
Muhammad Bilal <shiekhbilalbs5@gmail.com>
2025-04-23 16:17:00 +0500

    feat: react query dev tool installation

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"; 
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { MainLayout } from "./components/Layout/MainLayout";
 import { FetchOld } from "./Pages/FetchOld";
 import { FetchRQ } from "./Pages/FetchRQ";
@@ -32,6 +33,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router= {router}></RouterProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 };
